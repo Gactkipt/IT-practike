a09b03cbf271b48f289068e0947b335f7640b61c
Gactkipt <147954538+Gactkipt@users.noreply.github.com>
2023-10-18 21:52:16 +0300

    Update Разделение игроков на две команды.py

--- Лабораторная работа 1/Разделение игроков на две команды.py
+++ Лабораторная работа 1/Разделение игроков на две команды.py
@@ -1,8 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
 # TODO Разделите участников на две команды
-Ser = len(list_players) // 2
-One = list_players[:Ser]
-Two = list_players[Ser:]
+Seredina = len(list_players) // 2
+One = list_players[:Seredina]
+Two = list_players[Seredina:]
 print(One)
 print(Two)
