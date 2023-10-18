fc883fa5ba0db82778f39025d1ef100a897402b0
Gactkipt <147954538+Gactkipt@users.noreply.github.com>
2023-10-18 21:51:43 +0300

    Update Замена пропущенного элемента средним арифметическим.py

--- Лабораторная работа 1/Замена пропущенного элемента средним арифметическим.py
+++ Лабораторная работа 1/Замена пропущенного элемента средним арифметическим.py
@@ -1,12 +1,12 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-One = numbers[:4]
-Two = numbers[5:]
-Sum1 = sum(One)
-Sum2 = sum(Two)
-Girl = len(numbers)
-Man = Sum1 + Sum2
-Now = Man / Girl
+left_ = numbers[:4]
+right = numbers[5:]
+Sum_left = sum(left_)
+Sum_right = sum(right)
+long = len(numbers)
+Summa = Sum_left + Sum_right
+Now = Summa / long
 numbers[4] = Now
 print("Измененный список:", numbers)
